8e697d32d808b6bf24cc1661473e5f36985c1fa0
Salvatore Hakase <salvatorehakase@MacBook-Pro-de-Salvatore.local>
2024-08-06 14:54:07 -0300

    fix for sidebar styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className="dark">
-			<body>
+			<body className="text-white">
 				<Providers>{children}</Providers>
 			</body>
 		</html>
